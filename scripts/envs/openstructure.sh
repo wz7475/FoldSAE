a2646e937a9b11fc416fcb3983999dc457285663
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2025-04-16 09:55:00 +0200

    feat: e2e eval structures - support for differerent python envs and gpus

--- scripts/envs/openstructure.sh
+++ scripts/envs/openstructure.sh
@@ -2,6 +2,3 @@
 
 conda install zjack::openstructure
 pip install pandas
-
- cp /raid/battleamp_root/miniforge3/envs/openstructure/bin/tmscore \
-/raid/battleamp_root/miniforge3/bin/
--- scripts/protein-struct-pipe/eval_structures.sh
+++ scripts/protein-struct-pipe/eval_structures.sh
@@ -2,24 +2,29 @@
 
 experiment_dir="$1"
 rf_dir="$2"
+DEVICE_IDX_FOR_PYTHON_PROTEINMPNN=${3:-0}
+PYTHON_PROTEINMPNN=${4:-/home/wzarzecki/miniforge3/envs/bio_emb/bin/python}
+PYTHON_OPENSTRCUTERS=${5:-/home/wzarzecki/miniforge3/envs/openstruct/bin/python}
 
 # Inverse folding
 echo 'Running Protein-MPNN'
-sh scripts/protein-struct-pipe/protein_mpnn/run_inverse_folding.sh \
+CUDA_VISIBLE_DEVICES="$DEVICE_IDX_FOR_PYTHON_PROTEINMPNN" sh scripts/protein-struct-pipe/protein_mpnn/run_inverse_folding.sh \
     $rf_dir \
-    $experiment_dir &&
+    $experiment_dir \
+    "$PYTHON_PROTEINMPNN" &&
 
 # AF
 echo 'Running AF2'
 sh scripts/protein-struct-pipe/colabfold/run_colabfold.sh \
     $experiment_dir/seqs \
-    $experiment_dir/alphafold &&
+    $experiment_dir/alphafold  &&
 
 # openstructures
 echo 'Running OpenStructures eval'
 sh scripts/protein-struct-pipe/openstructures/evaluate_structures.sh \
     $rf_dir \
     $experiment_dir/alphafold \
-    $experiment_dir/structure_evaluation.csv
+    $experiment_dir/structure_evaluation.csv \
+    $PYTHON_OPENSTRCUTERS
 
 echo 'Done'
