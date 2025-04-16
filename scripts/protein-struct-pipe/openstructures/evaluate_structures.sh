f9e7f541387f6ecc5bb083f0c697c15310f0bf67
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2025-04-16 19:33:13 +0200

    feat WIP: script for autmated ablatiion - from RF to AF2 eval

--- scripts/protein-struct-pipe/openstructures/evaluate_structures.sh
+++ scripts/protein-struct-pipe/openstructures/evaluate_structures.sh
@@ -5,6 +5,10 @@ af_dir="$2"
 output_path="$3"
 PYTHON_EXEC=${4:-/raid/battleamp_root/miniforge3/envs/openstructure/bin/python3}
 
+# TMSCORE in python is just a wrapper for unix package installed through conda, it has to be added to path
+TMSCORE_EXEC_ROOT=$(dirname $PYTHON_EXEC)
+export PATH="$TMSCORE_EXEC_ROOT:$PATH";
+
 $PYTHON_EXEC scripts/protein-struct-pipe/openstructures/evaluate_structures.py \
         --rf_path $rf_dir \
         --af2_path $af_dir \
