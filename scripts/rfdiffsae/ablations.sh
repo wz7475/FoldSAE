3a792963747abaa5df3bf1f588b977b1bb9cd7ae
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2025-04-16 19:33:13 +0200

    feat WIP: script for autmated ablatiion - from RF to AF2 eval

--- scripts/rfdiffsae/ablations.sh
+++ scripts/rfdiffsae/ablations.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-# scritpt to generate ablations of main IterBlock for given range
+start_main=$1
+end_main=$2
+start_extra=$3
+end_extra=$4
+output_dir=${5:-/raid/rfdiffsae/ablated_outputs}
+PYTHON_EXEC=${6:-/home/wzarzecki/miniforge3/envs/rf124/bin/python}
 
-start=$1
-end=$2
-output_dir=${3:-/raid/rfdiffsae/ablated_outputs}
-PYTHON_EXEC=${4:-/home/wzarzecki/miniforge3/envs/rf124/bin/python}
-log_file="$output_dir/log.txt";
+mkdir -p "$output_dir"
 
-
-mkdir -p "$output_dir";
-touch "$log_file";
-
-for x in $(seq $start $end); do
-    $PYTHON_EXEC ./RFDiffSAE/scripts/run_inference.py \
-        "inference.output_prefix=$output_dir/ablations_main_iter_block_$x" \
-        "inference.num_designs=1" \
-        "inference.final_step=49" \
-        "contigmap.contigs=[100-200]" \
-        "model.skipped_main_block=$x"
-
-    echo "block no $x done" >> "$log_file";
+# main blocks ablated
+for x in $(seq $start_main $end_main); do
+	$PYTHON_EXEC ./RFDiffSAE/scripts/run_inference.py \
+		"inference.output_prefix=$output_dir/main_$x/pdb/ablations" \
+		"inference.num_designs=4" \
+		"inference.final_step=49" \
+		"contigmap.contigs=[100-200]" \
+		"model.skipped_main_block=$x"
 done
 
+# extra blocks ablated
+for x in $(seq $start_extra $end_main); do
+	$PYTHON_EXEC ./RFDiffSAE/scripts/run_inference.py \
+		"inference.output_prefix=$output_dir/extra_$x/pdb/ablations" \
+		"inference.num_designs=4" \
+		"inference.final_step=49" \
+		"contigmap.contigs=[100-200]" \
+		"model.skipped_extra_block=$x"
+done
 
+# no block ablated - reference
+$PYTHON_EXEC ./RFDiffSAE/scripts/run_inference.py \
+	"inference.output_prefix=$output_dir/reference/pdb/ablations" \
+	"inference.num_designs=4" \
+	"inference.final_step=49" \
+	"contigmap.contigs=[100-200]"
+
+# clean trb files
+find "$output_dir" -name "*.trb" -type f -delete
---
+++ scripts/rfdiffsae/ablations_and_eval.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+start_main=$1
+end_main=$2
+start_extra=$3
+end_extra=$4
+output_dir=${5:-/data/wzarzecki/ablation_test}
+PYTHON_RFDIFFUSION=${6:-/home/wzarzecki/miniforge3/envs/rf/bin/python}
+PYTHON_PROTEINMPNN=${7:-/home/wzarzecki/miniforge3/envs/bio_emb/bin/python}
+PYTHON_OPENSTRCUTERS=${8:-/home/wzarzecki/miniforge3/envs/openstruct/bin/python}
+
+
+# 1)
+# generate structures with RFDiff with ablated blocks
+# creates dir $output_dir/block_XXX/pdb for each block with pdb files
+#bash scripts/rfdiffsae/ablations.sh $start_main $end_main $start_extra $end_extra $output_dir $PYTHON_RFDIFFUSION
+
+# 2)
+# inverse-folding
+#for pdb_dir in $(find $output_dir -name "*pdb*" -type d); do
+#	block_dir=$(dirname $pdb_dir)
+#	CUDA_VISIBLE_DEVICES=1 bash scripts/protein-struct-pipe/protein_mpnn/run_inverse_folding.sh \
+#		$pdb_dir \
+#		$block_dir \
+#		$PYTHON_PROTEINMPNN
+#done
+
+# 3)
+# structures with AF2
+#for seq_dir in $(find $output_dir -name "*seqs*" -type d); do
+#	block_dir=$(dirname $seq_dir)
+#	CUDA_VISIBLE_DEVICES=0 bash scripts/protein-struct-pipe/colabfold/run_colabfold.sh \
+#		$seq_dir \
+#		"$block_dir/af2"
+#done
+
+# 4)
+# metrics with openstructures
+for RF_dir in $(find $output_dir -name "*pdb*" -type d); do
+	block_dir=$(dirname $RF_dir)
+	CUDA_VISIBLE_DEVICES=0 bash scripts/protein-struct-pipe/openstructures/evaluate_structures.sh \
+		$RF_dir \
+		"$block_dir/af2" \
+		$output_dir/structure_evaluation.csv \
+		$PYTHON_OPENSTRCUTERS
+done
