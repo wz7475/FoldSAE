ca9ddad332f7f1d6db1b74569a763bd69f5fe30f
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2025-04-30 17:43:22 +0200

    chore: adjust saa internvention and eval script

--- scripts/rfdiffsae/sae_and_eval.sh
+++ scripts/rfdiffsae/sae_and_eval.sh
@@ -28,16 +28,16 @@ PYTHON_BIOEMB=${7:-/home/wzarzecki/miniforge3/envs/bio_emb/bin/python}
 echo "generation of structures ..." ;
 structures_dir="$input_dir/pdb" ;
 # generate structure by RfDiffusion with SAE intervention
-#CUDA_VISIBLE_DEVICES=0 $PYTHON_RFDIFFUSION ./RFDiffSAE/scripts/run_inference.py inference.output_prefix="$structures_dir/$input_dir" \
-# 'contigmap.contigs=[100-200]' inference.num_designs="$num_designs" inference.final_step="$final_step"  \
-# saeinterventions=block4 ;
+CUDA_VISIBLE_DEVICES=2 SAE_DISABLE_TRITON=1 $PYTHON_RFDIFFUSION ./RFDiffSAE/scripts/run_inference.py inference.output_prefix="$structures_dir/" \
+ 'contigmap.contigs=[100-200]' inference.num_designs="$num_designs" inference.final_step="$final_step"  \
+ saeinterventions=block4 ;
 # keep only pdb
 rm "$structures_dir"/*.trb ;
 
 # 2)
 # inverse-folding with protein-mpnn sequences from structures - 1 sequence per 1 structure (checkout run_inverse_folding.sh)
 echo "inverse-folding to sequences ..."
-CUDA_VISIBLE_DEVICES=1 bash ./scripts/protein-struct-pipe/protein_mpnn/run_inverse_folding.sh "$structures_dir"  \
+CUDA_VISIBLE_DEVICES=2 bash ./scripts/protein-struct-pipe/protein_mpnn/run_inverse_folding.sh "$structures_dir"  \
   "$input_dir" "$PYTHON_PROTEINMPNN" ;
 # this script^^^ saves that to dir_given_as_second_arg/seqs
 sequences_dir="$input_dir/seqs"
@@ -46,7 +46,7 @@ sequences_dir="$input_dir/seqs"
 # new structures from sequences with AlphaFold2
 echo "new structures from sequences with AlphaFold2 ..."
 af2_dir="$input_dir/af2"
-CUDA_VISIBLE_DEVICES=1 bash scripts/protein-struct-pipe/colabfold/run_colabfold.sh \
+CUDA_VISIBLE_DEVICES=2 bash scripts/protein-struct-pipe/colabfold/run_colabfold.sh \
   $sequences_dir \
   $af2_dir
 
@@ -54,7 +54,7 @@ CUDA_VISIBLE_DEVICES=1 bash scripts/protein-struct-pipe/colabfold/run_colabfold.
 # comparison of RFDiff and AF2 structures
 echo "comparison of RFDiff and AF2 structures ..."
 results_file=$input_dir/structure_evaluation.csv
-CUDA_VISIBLE_DEVICES=1 bash scripts/protein-struct-pipe/openstructures/evaluate_structures.sh \
+CUDA_VISIBLE_DEVICES=2 bash scripts/protein-struct-pipe/openstructures/evaluate_structures.sh \
   $structures_dir \
   $af2_dir \
   $results_file \
@@ -64,7 +64,7 @@ echo "saved metrics to $results_file"
 # 5)
 echo "running bio_embeddings classifiers"
 classifiers_file="$input_dir/classifiers.csv"
-CUDA_VISIBLE_DEVICES=1 bash scripts/protein-struct-pipe/bio_emb/run_classifiers.sh \
+CUDA_VISIBLE_DEVICES=2 bash scripts/protein-struct-pipe/bio_emb/run_classifiers.sh \
   $sequences_dir \
   $classifiers_file \
   $PYTHON_BIOEMB
