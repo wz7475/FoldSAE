deb3384faade58bb2bbe44cab51dd9158f9fe90a
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2025-05-24 11:12:35 +0200

    sae latents ds: first generate activations saving every n-th timesetp, then genetae sae latents

--- scripts/sae/sae_latents_subcelluar_ds.sh
+++ scripts/sae/sae_latents_subcelluar_ds.sh
@@ -8,26 +8,35 @@ PYTHON_BIOEMB=${5:-/home/wzarzecki/miniforge3/envs/bio_emb/bin/python}
 DEVICE_IDX_FOR_OLD_GPU=${6:-1}
 PYTHON_RFDIFFUSION=${7:-/home/wzarzecki/miniforge3/envs/rf/bin/python}
 PYTHON_PROTEINMPNN=${8:-/home/wzarzecki/miniforge3/envs/bio_emb/bin/python}
+PYTHON_SAE=${9:-/home/wzarzecki/miniforge3/envs/diffsae/bin/python}
 
 dir_for_latents="$input_dir/latents"
+dir_for_activations="$input_dir/activations"
 dir_for_structures="$input_dir/structures"
 classifiers_file="$input_dir/classifiers.csv"
 
-# 1) generate structures and save SAEs' latents
+# 1) generate structures and save RFDiffusion activations
 echo "generation of structures ..." ;
 $PYTHON_RFDIFFUSION ./RFDiffSAE/scripts/run_inference.py \
     inference.output_prefix="$dir_for_structures/xxx" \
     'contigmap.contigs=[100-200]' \
     inference.num_designs=$num_of_structures \
     inference.final_step=1 \
-    saeinterventions=block4 \
-    saeinterventions.sae_pair_path=$sae_pair_path \
- 		saeinterventions.sae_non_pair_path=$sae_non_pair_path \
-    saeinterventions.sae_latents_base_dir=$dir_for_latents
+		activations=block4 \
+		activations.dataset_path=$dir_for_activations \
+		activations.keep_every_n_timestep=10 \
+		activations.keep_every_n_token=10
+
+# 2) generate latent with SAE
+echo "generating latents..."
+$PYTHON_SAE ./universal-diffsae/src/scripts/sae_latents_from_activations.py \
+		--base_dir $input_dir \
+		--sae_pair_path=$sae_pair_path \
+		--sae_non_pair_path=$sae_non_pair_path
 
 echo "used sae: $sae_pair_path\n $sae_non_pair_path" > "$input_dir/sae_paths.txt"
 
-# 2) inverse folding to sequences
+# 3) inverse folding to sequences
 echo "inverse-folding to sequences ..."
 sequences_dir="$input_dir/seqs"
 CUDA_VISIBLE_DEVICES="$DEVICE_IDX_FOR_OLD_GPU" bash scripts/protein-struct-pipe/protein_mpnn/run_inverse_folding.sh \
@@ -35,14 +44,14 @@ CUDA_VISIBLE_DEVICES="$DEVICE_IDX_FOR_OLD_GPU" bash scripts/protein-struct-pipe/
     $input_dir \
     "$PYTHON_PROTEINMPNN"
 
-# 5) running bio_embeddings classifiers
+# 4) running bio_embeddings classifiers
 echo "running bio_embeddings classifiers ..."
 CUDA_VISIBLE_DEVICES="$DEVICE_IDX_FOR_OLD_GPU" CUDA_VISIBLE_DEVICES=2 bash scripts/protein-struct-pipe/bio_emb/run_classifiers.sh \
   $sequences_dir \
   $classifiers_file \
   $PYTHON_BIOEMB
 
-# 6 "update datasets with labels from classifiers"
+# 5 "update datasets with labels from classifiers"
 echo "adding labels to HF datasets ..."
 $PYTHON_RFDIFFUSION scripts/sae/update_sae_latents_dataset.py \
 	--base-dir $input_dir
