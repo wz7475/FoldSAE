674a0f1027b40568b823eaef10055b022f24695f
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2025-06-01 18:24:54 +0200

    feat(sae intervention): load dict of SAEs with it's all depedencies from config

--- rfdiffusion/sae/interventionhook.py
+++ rfdiffusion/sae/interventionhook.py
@@ -2,43 +2,70 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 
 from rfdiffusion.Track_module import IterBlockOutput
-from rfdiffusion.sae.blockoutputtransformation import transform_from_iter_block_output, transform_to_iter_block_output
+from rfdiffusion.sae.blockoutputtransformation import (
+    transform_from_iter_block_output,
+    transform_to_iter_block_output,
+)
 from rfdiffusion.sae.universalsae import Sae
 
 
 class SAEInterventionHook:
-    def __init__(self, sae_for_pair: Sae, sae_for_non_pair: Sae, batch_size: int = 512,
-                 intervention_indices_for_pair: torch.Tensor | str = None,
-                 intervention_indices_for_non_pair: torch.Tensor | str = None,
-                 intervention_multiplier: float | None = None,
-                 ):
+    def __init__(
+        self,
+        sae_pair_path: str | None,
+        sae_non_pair_path: str | None,
+        batch_size: int = 512,
+        intervention_indices_for_pair: torch.Tensor | str = None,
+        intervention_indices_for_non_pair: torch.Tensor | str = None,
+        intervention_multiplier: float | None = None,
+    ):
         if torch.cuda.is_available():
-            self.device = torch.device('cuda')
+            self.device = torch.device("cuda")
         else:
-            self.device = torch.device('cpu')
-        self.sae_for_pair = sae_for_pair.to(self.device)
-        self.sae_for_non_pair = sae_for_non_pair.to(self.device)
+            self.device = torch.device("cpu")
+        self.sae_for_pair = Sae.load_from_disk(sae_pair_path, self.device).to(
+            self.device
+        ) if sae_pair_path else None
+        self.sae_for_non_pair = Sae.load_from_disk(sae_non_pair_path, self.device).to(
+            self.device
+        ) if sae_non_pair_path else None
         self.batch_size = batch_size
-        self.sae_for_pair.eval()
-        self.sae_for_non_pair.eval()
+        if self.sae_for_pair:
+            self.sae_for_pair.eval()
+        if self.sae_for_non_pair:
+            self.sae_for_non_pair.eval()
         self.intervention_multiplier = intervention_multiplier
-        self.intervention_indices_for_pair = intervention_indices_for_pair.to(
-            self.device) if intervention_indices_for_pair is not None else None
-        self.intervention_indices_for_non_pair = intervention_indices_for_non_pair.to(
-            self.device) if intervention_indices_for_non_pair is not None else None
+        self.intervention_indices_for_pair = (
+            torch.load(
+                intervention_indices_for_pair,
+                weights_only=True,
+            ).to(self.device)
+            if intervention_indices_for_pair is not None
+            else None
+        )
+        self.intervention_indices_for_non_pair = (
+            torch.load(
+                intervention_indices_for_non_pair,
+                weights_only=True,
+            ).to(self.device)
+            if intervention_indices_for_non_pair is not None
+            else None
+        )
         self.intervention_multiplier = intervention_multiplier
 
     @staticmethod
-    def _update_sae_latents(latents: torch.Tensor, indices: torch.Tensor, multiplier: int) -> torch.Tensor:
+    def _update_sae_latents(
+        latents: torch.Tensor, indices: torch.Tensor, multiplier: int
+    ) -> torch.Tensor:
         latents[:, indices] *= multiplier
         return latents
 
     def _reconstruct_batch_with_sae(
-            self,
-            sae: Sae,
-            batch: list[torch.Tensor],
-            indices_to_modify: torch.Tensor | None = None,
-            multiplier: int | None = None,
+        self,
+        sae: Sae,
+        batch: list[torch.Tensor],
+        indices_to_modify: torch.Tensor | None = None,
+        multiplier: int | None = None,
     ):
         batch = batch[0].to(self.device)
         sae_input, _, _ = sae.preprocess_input(batch.unsqueeze(1))
@@ -48,17 +75,23 @@ class SAEInterventionHook:
         latents = buf.scatter_(dim=-1, index=top_indices, src=top_acts)
         if indices_to_modify is not None and multiplier is not None:
             latents = self._update_sae_latents(latents, indices_to_modify, multiplier)
+            print("updated latents")
+        else:
+            print("not updated latens")
         return (latents @ sae.W_dec) + sae.b_dec, latents
 
-
     def _reconstruct_with_sae(
-            self,
-            output,
-            make_intervention: bool | None = None,
+        self,
+        output,
+        make_intervention: bool = False,
     ):
         pairs, non_pairs = transform_from_iter_block_output(output)
-        pairs_dataloader = DataLoader(TensorDataset(torch.stack(pairs)), self.batch_size)
-        non_pairs_dataloader = DataLoader(TensorDataset(torch.stack(non_pairs)), self.batch_size)
+        pairs_dataloader = DataLoader(
+            TensorDataset(torch.stack(pairs)), self.batch_size
+        )
+        non_pairs_dataloader = DataLoader(
+            TensorDataset(torch.stack(non_pairs)), self.batch_size
+        )
         reconstructed_pair_batches, reconstructed_non_pair_batches = [], []
         latents_pair_batches, latents_non_pair_batches = [], []
         if make_intervention:
@@ -70,21 +103,38 @@ class SAEInterventionHook:
             probes_non_pair_indices = None
             intervention_multiplier = None
         with torch.no_grad():
-            for batch in pairs_dataloader:
-                reconstruction, latents = self._reconstruct_batch_with_sae(self.sae_for_pair, batch,
-                                                                           probes_pair_indices, intervention_multiplier)
-                reconstructed_pair_batches.append(reconstruction)
-                latents_pair_batches.append(latents)
-            for batch in non_pairs_dataloader:
-                reconstruction, latents = self._reconstruct_batch_with_sae(self.sae_for_non_pair, batch,
-                                                                           probes_non_pair_indices,
-                                                                           intervention_multiplier)
-                reconstructed_non_pair_batches.append(reconstruction)
-                latents_non_pair_batches.append(latents)
-        reconstructed_pair_tensor = torch.cat(reconstructed_pair_batches, dim=0)
-        reconstructed_non_pair_tensor = torch.cat(reconstructed_non_pair_batches, dim=0)
-        return transform_to_iter_block_output(reconstructed_pair_tensor,
-                                              reconstructed_non_pair_tensor)
+            if self.sae_for_pair:
+                for batch in pairs_dataloader:
+                    reconstruction, latents = self._reconstruct_batch_with_sae(
+                        self.sae_for_pair,
+                        batch,
+                        probes_pair_indices,
+                        intervention_multiplier,
+                    )
+                    reconstructed_pair_batches.append(reconstruction)
+                    latents_pair_batches.append(latents)
+                reconstructed_pair_tensor = torch.cat(reconstructed_pair_batches, dim=0)
+            else:
+                reconstructed_pair_tensor = torch.stack(pairs).to(self.device)
+            if self.sae_for_non_pair:
+                for batch in non_pairs_dataloader:
+                    reconstruction, latents = self._reconstruct_batch_with_sae(
+                        self.sae_for_non_pair,
+                        batch,
+                        probes_non_pair_indices,
+                        intervention_multiplier,
+                    )
+                    reconstructed_non_pair_batches.append(reconstruction)
+                    latents_non_pair_batches.append(latents)
+                reconstructed_non_pair_tensor = torch.cat(
+                    reconstructed_non_pair_batches, dim=0
+                )
+            else:
+                reconstructed_non_pair_tensor = torch.stack(non_pairs).to(self.device)
+
+        return transform_to_iter_block_output(
+            reconstructed_pair_tensor, reconstructed_non_pair_tensor
+        )
 
     @torch.no_grad()
     def __call__(self, module, input, output):
